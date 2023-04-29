f21b6c5096d24683eb34b8d951732cd7c730efe8
ThomasCharuel <ThomasCharuel@users.noreply.github.com>
2023-04-29 17:02:02 +0200

    Implement basic CSS for about sections

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './index.module.scss';
+import AngleDown from '../../assets/angle-down.svg';
 
-function Collapse({ title, children }) {
+function Collapse({ title, children, isCollapsedInit = true }) {
+  const [isCollapsed, setIsCollapsed] = useState(isCollapsedInit);
+
   return (
-    <article className={styles.collapse}>
-      <h2 className={styles.collapse__title}>{title}</h2>
-      <div className={styles.collapse__content}>{children}</div>
+    <article className={styles.collapse + (!isCollapsed ? ` ${styles.active}` : '')}>
+      <header className={styles.collapse__header}>
+        <h2 className={styles.collapse__title}>{title}</h2>
+        <button onClick={() => setIsCollapsed(!isCollapsed)}>
+          <img src={AngleDown} className={styles.collapse__icon} />
+        </button>
+      </header>
+      {!isCollapsed && <div className={styles.collapse__content}>{children}</div>}
     </article>
   );
 }
@@ -14,6 +22,7 @@ function Collapse({ title, children }) {
 Collapse.propTypes = {
   title: PropTypes.string.isRequired,
   children: PropTypes.element.isRequired,
+  isCollapsedInit: PropTypes.bool,
 };
 
 export default Collapse;
