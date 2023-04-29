f89fe3221eb373022c5c034f98d27feb4f9f93f7
ThomasCharuel <ThomasCharuel@users.noreply.github.com>
2023-04-29 12:10:34 +0200

    Improve implementation API

--- src/utils/loaders/index.jsx
+++ src/utils/loaders/index.jsx
@@ -1,17 +1,22 @@
 import stays from '../../data/stays.json';
 
-// TODEV: Incorrect implementation
+async function getStays() {
+  return { stays };
+}
+
 async function getStay(stayId) {
+  const { stays } = await getStays();
   const stay = stays.filter((stay) => stay.id === stayId)[0];
 
-  return stay;
+  return { stay };
 }
 
 export async function staysLoader() {
+  const { stays } = await getStays();
   return { stays };
 }
 
 export async function stayLoader({ params }) {
-  const stay = await getStay(params.stayId);
+  const { stay } = await getStay(params.stayId);
   return { stay };
 }
