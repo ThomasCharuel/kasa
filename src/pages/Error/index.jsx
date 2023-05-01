168c7ed9cc2cdb9d28522c8510c7d1d0d8b88cd0
ThomasCharuel <ThomasCharuel@users.noreply.github.com>
2023-05-01 22:55:52 +0200

    Implement 404 error page

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './index.module.scss';
 
 export default function Error() {
   return (
-    <main>
-      <p>404</p>
-      <p>Oups! La page que vous demandez n&apos;existe pas.</p>
-      <Link to="/">Retourner sur la page d&apos;accueil</Link>
+    <main className={styles.main}>
+      <h1 className={styles.errorCode}>404</h1>
+      <p className={styles.errorMessage}>Oups! La page que vous demandez n&apos;existe pas.</p>
+      <Link to="/" className={styles.homePageLink}>
+        Retourner sur la page d&apos;accueil
+      </Link>
     </main>
   );
 }
