f3af76c302470ee1511bc953ad19d8626762a3e7
ThomasCharuel <ThomasCharuel@users.noreply.github.com>
2023-05-01 19:13:57 +0200

    Implement CSS footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <img src={kasaLogo} alt="Logo Kasa" className={styles.footer__logo} />
-      <p>© 2020 Kasa. All rights reserved</p>
+      <p className={styles.footer__text}>© 2020 Kasa. All rights reserved</p>
     </footer>
   );
 }
